6515d0b09c6e7d3dc17a3f5a8943eb3d15bd438c
IrelDev <ireldeveloper@gmail.com>
2020-07-07 14:49:22 +0500

    Add app delegate & link to other files in xcodeproj

--- XcodeCleaner/AppDelegate.swift
+++ XcodeCleaner/AppDelegate.swift
@@ -18,7 +18,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
         // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
-        let contentView = ContentView().environment(\.managedObjectContext, persistentContainer.viewContext)
+        let viewModel = ViewModel()
+        let contentView = ContentView().environment(\.managedObjectContext, persistentContainer.viewContext).environmentObject(viewModel)
 
         // Create the window and set the content view. 
         window = NSWindow(

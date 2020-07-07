6827098400e242791af40a4caf527ebe20fa13ca
IrelDev <ireldeveloper@gmail.com>
2020-07-07 19:23:03 +0500

    Add a 3-slice pie chart on initial directory sizes

--- XcodeCleaner/ViewModels/PieChartViewModel.swift
+++ XcodeCleaner/ViewModels/PieChartViewModel.swift
@@ -17,33 +17,51 @@ struct PieChartViewModel: PieChartViewModelProtocol {
             derivedDataSize += $0.size
         }
         
-        let derivedData = PieChartItemModel(value: Double(derivedDataSize), color: .pink)
-        
         var deviceSupportSize: Int64 = 0
         deviceSupport.forEach {
             deviceSupportSize += $0.size
         }
-        
-        let deviceSupport = PieChartItemModel(value: Double(deviceSupportSize), color: Color(.cyan))
         
         var archivesSize: Int64 = 0
         archives.forEach {
             archivesSize += $0.size
         }
         
-        let archives = PieChartItemModel(value: Double(archivesSize), color: .orange)
+        var derivedData: PieChartItemModel
+        var deviceSupport: PieChartItemModel
+        var archives: PieChartItemModel
         
-        let items = [archives, deviceSupport, derivedData]
-        self.items = items
+        if derivedDataSize == 0 && deviceSupportSize == 0 && archivesSize == 0 {
+            let defaultValue = 1.0
+            
+            derivedData = PieChartItemModel(value: defaultValue, color: .pink)
+            deviceSupport = PieChartItemModel(value: defaultValue, color: Color(.cyan))
+            archives = PieChartItemModel(value: defaultValue, color: .orange)
+            
+            let items = [archives, deviceSupport, derivedData]
+            self.items = items
+        } else {
+            derivedData = PieChartItemModel(value: Double(derivedDataSize), color: .pink)
+            deviceSupport = PieChartItemModel(value: Double(deviceSupportSize), color: Color(.cyan))
+            archives = PieChartItemModel(value: Double(archivesSize), color: .orange)
+
+            let items = [archives, deviceSupport, derivedData]
+            
+            var normalizedItems: [PieChartItemModel] = []
+            
+            items.forEach {
+                if $0.value != 0 {
+                    normalizedItems.append($0)
+                }
+            }
+            self.items = normalizedItems
+        }
     }
-    
     func getPCItems() -> PCItems {
         let pcItems = PCItems(items: [])
         
         items.forEach { item in
-            if item.value != 0 {
                 pcItems.items.append(item)
-            }
         }
         return pcItems
     }

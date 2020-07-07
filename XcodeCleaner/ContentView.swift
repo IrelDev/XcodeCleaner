1b101a57b5755ecb61052800989637c46cc537ab
IrelDev <ireldeveloper@gmail.com>
2020-07-07 13:02:33 +0500

    Initial Commit

---
+++ XcodeCleaner/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  XcodeCleaner
+//
+//  Created by Kirill Pustovalov on 07.07.2020.
+//  Copyright © 2020 Kirill Pustovalov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
